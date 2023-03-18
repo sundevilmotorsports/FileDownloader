aebae38309453705ddccd17e5dc080b11d733f8f
Joshua Tenorio <joshliam09@gmail.com>
2023-03-18 06:18:10 -0700

    handle logger printing all files

--- downloadFile.py
+++ downloadFile.py
@@ -5,9 +5,13 @@ import glob
 port = input("list port from discoverPorts.py: ")
 print(port)
 
+
 ser = serial.Serial(port)
 ser.flushInput()
 ser.write(b'h')
+
+"""
+file = open("savedData.csv", 'wb')
 while True:
     line = ser.readline()
     if line == b'Done reading\r\n':
@@ -15,6 +19,21 @@ while True:
         break
     else:
         file.write(line)
-
+file.close()
+"""
+num = 0
+file = None
+while True:
+    line = ser.readline()
+    if line == b'File:\r\n':
+        if file is not None:
+            file.close()
+        file = open(("data" + str(num) + ".csv"), "wb")
+        continue
+    elif line == b'Done reading\r\n':
+        print("exiting")
+        break
+    else:
+        file.write(line)
 
 file.close()
